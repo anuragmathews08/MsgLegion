a2cb1b1006a53a580235a9349e64a23f029d2707
Anurag Mathews <anuragmathews619@gmail.com>
2019-02-24 16:19:28 +0530

    login | register page created
    created login and register page design for the application.

--- main.js
+++ main.js
@@ -5,22 +5,35 @@ const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const path = require("path");
 const url = require("url");
+const Menu = electron.Menu;
 
 let win;
 
 function createWindow(){
-    win = new BrowserWindow();
+    win = new BrowserWindow({minWidth: 970 , minHeight: 745 , show: false});
     win.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html'),
         protocol: 'file',
         slashes: true
     }));
 
-    win.webContents.openDevTools();
+    // win.webContents.openDevTools();
+
+    win.once('ready-to-show', () => {
+        win.show()
+    });
+
     win.on('closed', () => {
         win = null;
     });
-    
+
+        
 }
 
-app.on('ready', createWindow);
+
+app.on('ready', function(){
+    createWindow();
+    const menu  = null;
+    Menu.setApplicationMenu(menu);
+});
+
